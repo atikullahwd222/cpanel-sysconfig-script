d8ec298e91eae70e5578514fff2002fbbb44dfb3
atikullahwd222 <atikullahwd90@gmail.com>
2025-02-02 20:16:05 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,40 +13,65 @@ prompt_input() {
 }
 
 # Display installation options
-echo "==================================================================="
-echo "// Select an installation option:                                //"
-echo "// 1. Install WHM                                                //"
-echo "// 2. Initialize Theme4Sell                                      //"
-echo "// 3. Activate or Fix WHM Lic. with Theme4Sell                   //"
-echo "// 4. Install and Active LiteSpeedX                              //"
-echo "// 5. Tweak Settings                                             //"
-echo "// 6. Install Softaculous                                        //"
-echo "// 7. Active Softaculous                                         //"
-echo "// 8. Install and active Jetbackup                               //"
-echo "// 9. Install and active Whmreseller                             //"
-echo "// 0. Fresh install with Theme4Sell                              //"
-echo "=================================================================="
-read -p "Enter your choice (0-9): " choice
+echo -e "${GREEN}#    ____  _    _    _____           _                 "
+echo -e "${GREEN}#   |  _ \| |  | |  / ____|         | |                "
+echo -e "${GREEN}#   | |_) | |__| | | (___  _   _ ___| |_ ___ _ __ ___  "
+echo -e "${GREEN}#   |  _ <|  __  |  \___ \| | | / __| __/ _ \ '_ ` _ \ "
+echo -e "${GREEN}#   | |_) | |  | |  ____) | |_| \__ \ ||  __/ | | | | |"
+echo -e "${GREEN}#   |____/|_|  |_| |_____/ \__, |___/\__\___|_| |_| |_|"
+echo -e "${GREEN}#        /_ | |__ \         __/ |                      "
+echo -e "${GREEN}#   __   _| |    ) |       |___/                       "
+echo -e "${GREEN}#   \ \ / / |   / /                                    "
+echo -e "${GREEN}#    \ V /| |_ / /_                                    "
+echo -e "${GREEN}#     \_/ |_(_)____|                                   "
+echo -e "${GREEN}#                                                       "
+echo -e "${GREEN}#${NC}"
+                                                  
+
+echo "=================== BH System v1.2 ============================"
+echo "Select an installation option:                                "
+echo "${GREEN} 1. ${NC} Install WHM                                                "
+echo "${GREEN} 2. ${NC} Initialize Theme4Sell                                      "
+echo "${GREEN} 3. ${NC} Activate or Fix WHM Lic. with Theme4Sell                   "
+echo "${GREEN} 4. ${NC} Install and Active LiteSpeedX                              "
+echo "${GREEN} 5. ${NC} Tweak Settings                                             "
+echo "${GREEN} 6. ${NC} Install Softaculous                                        "
+echo "${GREEN} 7. ${NC} Active Softaculous                                         "
+echo "${GREEN} 8. ${NC} Install and active Jetbackup                               "
+echo "${GREEN} 9. ${NC} Install and active Whmreseller                             "
+echo "${GREEN} 10. ${NC} Install and Active sitepad                                "
+echo "${GREEN} 11. ${NC} Install and Active Im360                                  "
+echo "${GREEN} 12. ${NC} Install and CSF                                           "
+echo "${GREEN} 13. ${NC} Install Cloudlinux                                        "
+echo "${GREEN} 14. ${NC} Install Enable Cloudlinux                                 "
+echo "${GREEN} 0. ${NC} Fresh install with Theme4Sell                              "
+echo "=================== BH System v1.2 ============================"
+read -p "Enter your choice (0-3): " choice
 
 if [[ "$choice" == "1" ]]; then
-    server_ip=$(prompt_input "Enter the server IP")
-    hostname=$(prompt_input "Enter the hostname")
-    hostname_prefix=$(prompt_input "Enter the hostname prefix")
+    echo "This script will configure system settings and install cPanel."
+    read -p "Do you want to continue? (y/n): " confirm_choice
+    if [[ "$confirm_choice" == "y" ]]; then
+        server_ip=$(prompt_input "Enter the server IP")
+        hostname=$(prompt_input "Enter the hostname")
+        hostname_prefix=$(prompt_input "Enter the hostname prefix")
 
-    echo "$server_ip $hostname $hostname_prefix" | sudo tee -a /etc/hosts
+        echo "$server_ip $hostname $hostname_prefix" | sudo tee -a /etc/hosts
 
-    echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
-    echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+        echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+        echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
 
-    yum install nano -y
-    yum update -y
-    yum install almalinux-release -y
-    iptables-save > ~/firewall.rules
-    systemctl stop firewalld.service
-    systemctl disable firewalld.service
-
-    cd /home
-    curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
+        yum install nano -y
+        yum update -y
+        yum install almalinux-release -y
+        iptables-save > ~/firewall.rules
+        systemctl stop firewalld.service
+        systemctl disable firewalld.service
+    fi
+    else
+        cd /home
+        curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
+    fi
 
 elif [[ "$choice" == "2" ]]; then
     curl -sL https://repo.magicbyte.pw/init.sh | sudo bash -
@@ -78,10 +103,11 @@ elif [[ "$choice" == "5" ]]; then
     whmapi1 set_tweaksetting key=resetpass value=0
 
     whmapi1 set_tweaksetting key=resetpass_sub value=0
-    
-    mkdir /etc/cpanel/ea4/profiles/custom
 
-    sudo wget https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-script/refs/heads/main/EasyApache4-BH-Custome.json -O /etc/cpanel/ea4/profiles/custom/EasyApache4-BH-Custome.json
+    whmapi1 set_tweaksetting key=allowremotedomains value=1
+
+    mkdir /etc/cpanel/ea4/profiles/custom
+    curl -o /etc/cpanel/ea4/profiles/custom/EasyApache4-BH-Custome.json https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-script/refs/heads/main/EasyApache4-BH-Custome.json
 
     echo "${GREEN} Tweak settings Successfull ${NC}"
     echo "===================================================================="
@@ -191,6 +217,7 @@ elif [[ "$choice" == "0" ]]; then
 
         whmapi1 set_tweaksetting key=resetpass_sub value=0
 
+        whmapi1 set_tweaksetting key=allowremotedomains value=1
 
         mkdir /etc/cpanel/ea4/profiles/custom
         curl -o /etc/cpanel/ea4/profiles/custom/EasyApache4-BH-Custome.json https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-script/refs/heads/main/EasyApache4-BH-Custome.json
