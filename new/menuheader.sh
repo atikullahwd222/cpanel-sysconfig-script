9f60674b3102d0f46c41d8920d4c130a84e99cd1
Atikullah Wd <atikullahwd90@gmail.com>
2025-08-30 22:44:54 +0600

    Add server config automation and spinner utility
    Automates basic server configuration in main_menu.sh, including hosts file, DNS, package installation, timezone, and firewall setup. Adds a spinner function in menuheader.sh to indicate progress during background operations.

--- new/menuheader.sh
+++ new/menuheader.sh
@@ -12,6 +12,19 @@ SCRIPT_URI="https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-sc
 SCRIPT_VERSION="1.0.0"
 TOOLS_VERSION="1.0.0"
 
+spinner() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ -d /proc/$pid ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
 
 # Width of the menu
 WIDTH=50
