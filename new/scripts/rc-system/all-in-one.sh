2ceae0d2a63a506b54d55b0775fc9878e91c1eb3
Atikullah Wd <atikullahwd90@gmail.com>
2025-08-30 19:12:30 +0600

    Update all-in-one.sh

--- new/scripts/rc-system/all-in-one.sh
+++ new/scripts/rc-system/all-in-one.sh
@@ -88,73 +88,108 @@ main() {
 
     read -p "$(echo -e ${YELLOW}Enter your choices separated by space: ${NC})" selections
 
-    for choice in $selections; do
-        case $choice in
-            1)
-                show_progress "Removing existing license"
-                check_command "wget -q -O remover https://mirror.resellercenter.ir/remover && chmod +x remover && ./remover" "License removal"
-                ;;
-            2)
-                show_progress "Installing cPanel VPS"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) cPanel && RcLicenseCP && RcLicenseCP -fleetssl && /scripts/configure_firewall_for_cpanel" "cPanel installation"
-                ;;
-            3)
-                show_progress "Installing LiteSpeed License"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) liteSpeed && RcLicenseLSWS" "LiteSpeed installation"
-                ;;
-            4)
-                show_progress "Installing LiteSpeed Load Balancer"
-                check_command "RCUpdate lslb && bash <(curl -s https://mirror.resellercenter.ir/pre.sh) LSLB && RcLSLB" "LiteSpeed Load Balancer installation"
-                ;;
-            5)
-                show_progress "Installing Softaculous"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) Softaculous && RcLicenseSoftaculous" "Softaculous installation"
-                ;;
-            6)
-                show_progress "Installing JetBackup"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) JetBackup && RcLicenseJetBackup" "JetBackup installation"
-                ;;
-            7)
-                show_progress "Installing WHMReseller"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) WHMReseller && RcLicenseWHMReseller" "WHMReseller installation"
-                ;;
-            8)
-                show_progress "Installing Imunify360"
-                check_command "wget -q https://repo.imunify360.cloudlinux.com/defence360/i360deploy.sh && bash i360deploy.sh && bash <(curl -s https://mirror.resellercenter.ir/pre.sh) Imunify360 && RcLicenseImunify360" "Imunify360 installation"
-                ;;
-            9)
-                show_progress "Installing cPGuard"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) CPGuard && RcLicenseCPGuard" "cPGuard installation"
-                ;;
-            10)
-                show_progress "Installing Da-Reseller"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) DAReseller && RcLicenseDAReseller" "Da-Reseller installation"
-                ;;
-            11)
-                show_progress "Installing OSM"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) OSM && RcLicenseOSM" "OSM installation"
-                ;;
-            12)
-                show_progress "Installing CXS"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) CXS && RcLicenseCXS" "CXS installation"
-                ;;
-            13)
-                show_progress "Installing CloudLinux"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) CloudLinux && RcLicenseCLN && t4srcCLN -install" "CloudLinux installation"
-                ;;
-            14)
-                show_progress "Installing SitePad"
-                check_command "bash <(curl -s https://mirror.resellercenter.ir/pre.sh) Sitepad && RcLicenseSitepad" "SitePad installation"
-                ;;
-            0)
-                echo -e "${GREEN}Exiting...${NC}"
-                exit 0
-                ;;
-            *)
-                echo -e "${RED}Invalid choice: $choice${NC}"
-                ;;
-        esac
-    done
+    echo "===================================================================================================="
+    remove_license=$(prompt_input "Do you want to remove the existing license? (y/n)")
+    install_cpanel=$(prompt_input "Do you want to install cPanel VPS ${YELLOW}(Select Carefully)${NC}? (y/n)")
+    # install_dedicated=$(prompt_input "Do you want to install Cpanel Dedicated ${YELLOW}(Select Carefully)${NC}? (y/n)")
+    install_litespeed=$(prompt_input "Do you want to install and activate LiteSpeed License? (y/n)")
+    install_litespeed_lb=$(prompt_input "Do you want to install and activate LiteSpeed Load Balancer? (y/n)")
+    install_softaculous=$(prompt_input "Do you want to install Softaculous? (y/n)")
+    install_jetbackup=$(prompt_input "Do you want to install JetBackup? (y/n)")
+    install_whmreseller=$(prompt_input "Do you want to install WHMReseller? (y/n)")
+    install_im360=$(prompt_input "Do you want to install Imunify360 ${YELLOW}(Select Carefully)${NC}? (y/n)")
+    install_cpguard=$(prompt_input "Do you want to install cPGuard ${YELLOW}(Select Carefully)${NC}? (y/n)")
+    install_dareseller=$(prompt_input "Do you want to install Da-Reseller? (y/n)")
+    install_osm=$(prompt_input "Do you want to install OSM? (y/n)")
+    install_cxs=$(prompt_input "Do you want to install CXS? (y/n)")
+    install_cloudlinux=$(prompt_input "Do you want to install CloudLinux? (y/n)")
+    install_sitepad=$(prompt_input "Do you want to install SitePad? (y/n)")
+    echo "===================================================================================================="
+
+    :
+
+
+    echo "Do you want to proceed with the installation? (y/n)"
+    read proceed
+
+
+    if [[ "$remove_license" == "y" ]]; then
+        wget -q -O remover https://mirror.resellercenter.ir/remover && chmod +x remover && ./remover >/dev/null 2>&1
+    fi
+    # Installing cPanel
+    if [[ "$install_cpanel" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) cPanel >/dev/null 2>&1; RcLicenseCP >/dev/null 2>&1
+        RcLicenseCP -fleetssl >/dev/null 2>&1
+        /scripts/configure_firewall_for_cpanel >/dev/null 2>&1
+        /usr/local/cpanel/cpsrvd >/dev/null 2>&1
+        iptables -P INPUT ACCEPT >/dev/null 2>&1
+        iptables -P FORWARD ACCEPT >/dev/null 2>&1
+        iptables -P OUTPUT ACCEPT >/dev/null 2>&1
+        iptables -t nat -F >/dev/null 2>&1
+        iptables -t mangle -F >/dev/null 2>&1
+        /usr/sbin/iptables -F >/dev/null 2>&1
+        /usr/sbin/iptables -X >/dev/null 2>&1
+        bash <(curl -fsSL https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-script/refs/heads/main/tweak.sh) &>/dev/null
+    fi
+
+
+    if [[ "$install_litespeed_lb" == "y" ]]; then
+        RCUpdate lslb >/dev/null 2>&1
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) LSLB >/dev/null 2>&1; RcLSLB >/dev/null 2>&1
+    fi
+
+    # Installing and enabling LiteSpeedX
+    if [[ "$install_litespeed" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) liteSpeed >/dev/null 2>&1; RcLicenseLSWS >/dev/null 2>&1
+    fi
+
+    # Installing and enabling Softaculous
+    if [[ "$install_softaculous" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) Softaculous >/dev/null 2>&1; RcLicenseSoftaculous >/dev/null 2>&1
+    fi
+
+    # Installing and enabling JetBackup
+    if [[ "$install_jetbackup" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) JetBackup >/dev/null 2>&1; RcLicenseJetBackup >/dev/null 2>&1
+    fi
+
+    # Installing and enabling WHMReseller
+    if [[ "$install_whmreseller" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) WHMReseller >/dev/null 2>&1; RcLicenseWHMReseller >/dev/null 2>&1
+    fi
+
+    # Installing and enabling Imunify360
+    if [[ "$install_im360" == "y" ]]; then
+        wget -q https://repo.imunify360.cloudlinux.com/defence360/i360deploy.sh && bash i360deploy.sh >/dev/null 2>&1
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) Imunify360 >/dev/null 2>&1; RcLicenseImunify360 >/dev/null 2>&1
+    fi
+
+    # Installing and enabling CloudLinux
+    if [[ "$install_cloudlinux" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) CloudLinux >/dev/null 2>&1; RcLicenseCLN >/dev/null 2>&1
+        t4srcCLN -install >/dev/null 2>&1
+    fi
+
+    # Installing and enabling SitePad
+    if [[ "$install_sitepad" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) Sitepad >/dev/null 2>&1; RcLicenseSitepad >/dev/null 2>&1
+    fi
+
+    if [[ "$install_cpguard" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) CPGuard >/dev/null 2>&1; RcLicenseCPGuard >/dev/null 2>&1
+    fi
+
+    if [[ "$install_dareseller" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) DAReseller >/dev/null 2>&1; RcLicenseDAReseller >/dev/null 2>&1
+    fi
+
+    if [[ "$install_osm" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) OSM >/dev/null 2>&1; RcLicenseOSM >/dev/null 2>&1
+    fi
+
+    if [[ "$install_cxs" == "y" ]]; then
+        bash <( curl https://mirror.resellercenter.ir/pre.sh ) CXS >/dev/null 2>&1; RcLicenseCXS >/dev/null 2>&1
+    fi
 
     echo -e "${GREEN}All selected tasks completed!${NC}"
 }
