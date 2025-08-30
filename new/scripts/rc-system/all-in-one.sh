5f8de32b640c4d1304f629a96e8385f4201b6cb4
Atikullah Wd <atikullahwd90@gmail.com>
2025-08-30 19:02:15 +0600

    Update all-in-one.sh

--- new/scripts/rc-system/all-in-one.sh
+++ new/scripts/rc-system/all-in-one.sh
@@ -2,13 +2,6 @@
 # Constants
 HEADER_URL="https://raw.githubusercontent.com/atikullahwd222/cpanel-sysconfig-script/refs/heads/main/new/menuheader.sh"
 
-# Color definitions
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
 # Function to display header
 display_header() {
     clear
