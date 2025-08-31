fa3606139dc680b8ce4246d524e19ebf003ac093
Atikullah Wd <atikullahwd90@gmail.com>
2025-08-31 16:33:56 +0600

    Update ini.sh

--- new/scripts/ini.sh
+++ new/scripts/ini.sh
@@ -39,7 +39,7 @@ apply_changes() {
         echo "${GREEN}Backup created: ${YELLOW}$ini_file.bak${RESET}"
         
         sed -i 's/^zlib.output_compression = .*/zlib.output_compression = On/' "$ini_file"
-        sed -i 's/^disable_functions =.*/disable_functions = show_source, system, shell_exec, passthru, exec, mail/' "$ini_file"
+        sed -i 's/^disable_functions =.*/disable_functions = show_source, system, shell_exec, passthru, exec/' "$ini_file"
         sed -i 's/^max_execution_time = .*/max_execution_time = 3000/' "$ini_file"
         sed -i 's/^max_input_time = .*/max_input_time = 6000/' "$ini_file"
         sed -i 's/^;*max_input_vars =.*/max_input_vars = 1000/' "$ini_file"
